acc37ef6d893662b2b167db7024e4a0c5d9bf4e3
Diego Kuwahara <diegookc@hotmail.com>
2018-10-30 11:04:07 -0200

    Adição inicial de Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppHeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
     FooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
